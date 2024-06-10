0bf814dff656fc2e6b682d6e6d88211d68b198cc
Vladimir Shchukin <91376232+madfcat@users.noreply.github.com>
2024-06-10 16:30:15 +0300

    Change ft_filter to class implementation python0 ex06

--- python0/ex06/ft_filter.py
+++ python0/ex06/ft_filter.py
@@ -1,34 +1,20 @@
-# def comprehend(func, x):
-#     if func and func(x):
-#         return x
-#     elif not func and bool(x):
-#         return x
-
-# def ft_filter(func, iterable):
-#     """
-# filter(function or None, iterable) --> filter object
-
-# Return an iterator yielding those items of iterable for which function(item)
-# is true. If function is None, return the items that are true.
-#     """
-
-#     result = [comprehend(func, x) for x in iterable]
-
-#     return result
-
-def ft_filter(func, iterable):
+class ft_filter:
     """
 filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.
     """
-    result = []
 
-    for x in iterable:
-        if func and func(x):
-            result.append(x)
-        elif not func and bool(x):
-            result.append(x)
+    def __init__(self, function, iterable):
+        self.function = function if function is not None else lambda x: x
+        self.iterator = iter(iterable)
 
-    return result
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            item = next(self.iterator)  # Get the next item from the iterator
+            if self.function(item):     # Apply the filter function
+                return item
