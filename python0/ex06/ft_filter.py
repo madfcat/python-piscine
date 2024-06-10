a429e637cc47de1f3c0e8d2d6a5459da4db759a2
Vladimir Shchukin <91376232+madfcat@users.noreply.github.com>
2024-06-10 16:49:41 +0300

    Change ft_filter to list comprehension

--- python0/ex06/ft_filter.py
+++ python0/ex06/ft_filter.py
@@ -7,14 +7,11 @@ is true. If function is None, return the items that are true.
     """
 
     def __init__(self, function, iterable):
-        self.function = function if function is not None else lambda x: x
-        self.iterator = iter(iterable)
+        self.filtered_items = (x for x in iterable if function(x)) if function is not None else (x for x in iterable if x)
+        self.iterator = iter(self.filtered_items)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        while True:
-            item = next(self.iterator)  # Get the next item from the iterator
-            if self.function(item):     # Apply the filter function
-                return item
+        return next(self.iterator)
