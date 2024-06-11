ead9f0f1f53e3fcc01115e30629ffea58ce62897
Vladimir Shchukin <91376232+madfcat@users.noreply.github.com>
2024-06-11 15:41:16 +0300

    Add python1 ex04 and improve

--- python1/ex03/zoom.py
+++ python1/ex03/zoom.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from load_image import ft_load
 import matplotlib.pyplot as plt
@@ -13,6 +12,8 @@ import matplotlib.pyplot as plt
 #     return np.stack([grayscale], axis=-1)
 
 def rgb_to_gray(rgb):
+    """
+    """
     gray = np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140]).astype(np.uint8)
     return np.stack([gray], axis=-1)
 
@@ -39,25 +40,29 @@ def rgb_to_gray(rgb):
 def zoom(image):
     """
     """
-    # new_image = convert_rgb_to_grayscale(image)
-    new_image = rgb_to_gray(image)
-    new_image = new_image[100:500, 440:840]
-    
-    shape_short = list(new_image.shape)
-    shape_short.pop()
-    print("New shape after slicing:", new_image.shape, " or ", tuple(shape_short))
+    new_image = image[100:500, 440:840]
 
     return new_image
 
 
 def main():
-    image = ft_load("animal.jpeg")
-    print(image)
-    new_image = zoom(image)
-    print(new_image)
-    plt.imshow(new_image, cmap='gray')
-    plt.show()
+    try:
+        image = ft_load("animal.jpeg")
+        print(image)
+        
+        new_image = rgb_to_gray(image)
+        new_image = zoom(new_image)
+        
+        shape_short = list(new_image.shape)
+        shape_short.pop()
+        print("New shape after slicing:", new_image.shape, " or ", tuple(shape_short))
+        print(new_image)
 
+        plt.imshow(new_image, cmap='gray')
+        plt.show()
+
+    except Exception as e:
+        print(f"{type(e).__name__}: {e}")
 
 if __name__ == "__main__":
     main()
