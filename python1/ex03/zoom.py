7b82fa88ae2443fe145f8dd997ba73837529ad07
Vladimir Shchukin <91376232+madfcat@users.noreply.github.com>
2024-06-11 15:08:13 +0300

    Fix gray for python1 ex03

--- python1/ex03/zoom.py
+++ python1/ex03/zoom.py
@@ -4,34 +4,37 @@ from load_image import ft_load
 import matplotlib.pyplot as plt
 
 # def convert_rgb_to_grayscale(image):
+#     """
+#     """
 #     # Apply the formula ([0] + [1] + [2]) / 3 to each 1-D slice along axis 2
 #     grayscale =  np.apply_along_axis(lambda x: np.mean(x), axis=2, arr=image).astype(np.uint8)
 
 #     # Stack the grayscale values along the third axis to create a 3-channel grayscale image
 #     return np.stack([grayscale], axis=-1)
 
-# def rgb2gray(rgb):
-#     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+def rgb_to_gray(rgb):
+    gray = np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140]).astype(np.uint8)
+    return np.stack([gray], axis=-1)
 
 
-def rgb_to_gray(img):
-    """"""
-    grayImage = np.zeros(img.shape)
-    R = np.array(img[:, :, 0])
-    G = np.array(img[:, :, 1])
-    B = np.array(img[:, :, 2])
+# def rgb_to_gray(img):
+#     """"""
+#     grayImage = np.zeros(img.shape)
+#     R = np.array(img[:, :, 0])
+#     G = np.array(img[:, :, 1])
+#     B = np.array(img[:, :, 2])
 
-    R = (R *.299)
-    G = (G *.587)
-    B = (B *.114)
+#     R = (R *.299)
+#     G = (G *.587)
+#     B = (B *.114)
 
-    Avg = (R+G+B)
-    grayImage = img.copy()
+#     Avg = (R+G+B)
+#     grayImage = img.copy()
 
-    for i in range(3):
-        grayImage[:,:,i] = Avg
+#     for i in range(3):
+#         grayImage[:,:,i] = Avg
 
-    return grayImage
+#     return grayImage
 
 def zoom(image):
     """
@@ -40,7 +43,9 @@ def zoom(image):
     new_image = rgb_to_gray(image)
     new_image = new_image[100:500, 440:840]
     
-    print("New shape after slicing:", new_image.shape)
+    shape_short = list(new_image.shape)
+    shape_short.pop()
+    print("New shape after slicing:", new_image.shape, " or ", tuple(shape_short))
 
     return new_image
 
@@ -50,7 +55,7 @@ def main():
     print(image)
     new_image = zoom(image)
     print(new_image)
-    plt.imshow(new_image)
+    plt.imshow(new_image, cmap='gray')
     plt.show()
 
 
