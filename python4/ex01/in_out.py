6660d917ff7d625a4fd98809f78f874cc0ae5581
Vladimir Shchukin <91376232+madfcat@users.noreply.github.com>
2024-06-13 19:32:27 +0300

    Add python4 ex01

---
+++ python4/ex01/in_out.py
@@ -0,0 +1,18 @@
+def square(x: int | float) -> int | float:
+    return x ** 2
+
+
+def pow(x: int | float) -> int | float:
+    return x ** x
+
+
+def outer(x: int | float, function) -> object:
+    count = 0
+
+    def inner() -> float:
+        nonlocal x
+        nonlocal count
+        x = function(x)
+        return x
+
+    return inner
