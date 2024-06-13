3a53f6f1a72a4eb3fac8c7e7ffc420c1d212778a
Vladimir Shchukin <91376232+madfcat@users.noreply.github.com>
2024-06-13 20:24:33 +0300

    Norminette, move .flake

--- python4/ex00/tester.py
+++ python4/ex00/tester.py
@@ -5,7 +5,8 @@ ft_statistics(1, 42, 360, 11, 64, toto="mean", tutu="median", tata="quartile")
 print("-----")
 ft_statistics(5, 75, 450, 18, 597, 27474, 48575, hello="std", world="var")
 print("-----")
-ft_statistics(5, 75, 450, 18, 597, 27474, 48575, ejfhhe="heheh", ejdjdejn="kdekem")
+ft_statistics(5, 75, 450, 18, 597, 27474, 48575, ejfhhe="heheh",
+              ejdjdejn="kdekem")
 print("-----")
 ft_statistics(toto="mean", tutu="median", tata="quartile")
 
@@ -17,8 +18,6 @@ ft_statistics(toto="mean", tutu="median", tata="quartile")
 # y = quantiles(nums, n=4)[2]
 # print("Quartile from numpy:", x, y)
 # print("-----")
-
-
 
 # nums = [1, 42, 360, 11]
 # ft_statistics(1, 42, 360, 11, toto="mean", tutu="median", tata="quartile")
